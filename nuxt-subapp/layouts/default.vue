6ea71555b58148dfbcf520b69ea6b2ccddb09d4a
Antony Budianto <antony.budianto@mekari.com>
2024-09-30 17:15:42 +0700

    initial

---
+++ nuxt-subapp/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nuxt-app">
+    <header>
+      <Logo />
+      <h1 class="title">nuxt-sub-app 123</h1>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.nuxt-app {
+  margin: 0 auto;
+  width: 400px;
+}
+</style>
+<script>
+export default {
+  mounted() {
+    console.log("subapp:", location.host);
+  },
+};
+</script>
